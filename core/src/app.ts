16ac57c331bc6f68a285ec5f5187ecded6505ac0
RifatNR <Rifatnoor92@gmail.com>
2024-10-25 15:29:14 +0600

    ok base query setup is working

--- core/src/app.ts
+++ core/src/app.ts
@@ -1,11 +1,24 @@
 import express, { Request, Response } from "express";
 import { test } from "@/folder/test.js";
+import { connectDB, pool } from "@/database/db";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/connect", async (req: Request, res: Response) => {
+    await connectDB();
     res.send("Hello, Context lagbe!!");
+});
+
+app.get("/select", async (req: Request, res: Response) => {
+    try {
+        await connectDB();
+        const result = await pool.query("SELECT * FROM ghosts");
+        console.log("Fetched Users:", result.rows);
+    } catch (err) {
+        console.error("Error fetching data:", err);
+    }
+    res.json("Hello, Context lagbe!!");
 });
 
 // Start the server
