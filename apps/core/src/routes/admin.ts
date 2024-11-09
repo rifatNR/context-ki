cc881575ed7258e344ed25bd2da454f39806ac23
RifatNR <Rifatnoor92@gmail.com>
2024-11-09 21:34:51 +0600

    experimenting

--- apps/core/src/routes/admin.ts
+++ apps/core/src/routes/admin.ts
@@ -3,44 +3,47 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/context", async (req, res) => {
+router.get("/idea", async (req, res) => {
     try {
-        const result = await pool.query("SELECT * FROM contexts");
+        const result = await pool.query("SELECT * FROM ideas");
         console.log("Fetched:", result.rows);
         res.status(200).json(result.rows);
     } catch (error) {
         console.error("Error fetching data:", error);
         res.status(500).json({
             error,
-            msg: "An error occurred while getting the context.",
+            msg: "An error occurred while getting all ideas.",
         });
     }
 });
 
-router.post("/context/create", async (req, res) => {
-    const { title, context, details, image } = req.body || {};
+router.post("/idea/create", async (req, res) => {
+    const { title, subtitle, details, image, claimed_date } = req.body || {};
 
     const createdAt = new Date();
+    const updated_at = new Date();
     const query = `
-      INSERT INTO contexts (title, context, details, image, created_at)
-      VALUES ($1, $2, $3, $4, $5)
+      INSERT INTO ideas (title, subtitle, details, image, claimed_date, created_at, updated_at)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
       RETURNING *;
     `;
 
     try {
         const result = await pool.query(query, [
             title,
-            context,
+            subtitle || null,
             details || null,
             image || null,
+            claimed_date || null,
             createdAt,
+            updated_at,
         ]);
         res.status(201).json(result.rows[0]);
     } catch (error) {
-        console.error("Error inserting context:", error);
+        console.error("Error inserting idea:", error);
         res.status(500).json({
             error,
-            msg: "An error occurred while creating the context.",
+            msg: "An error occurred while creating the idea.",
         });
     }
 });
