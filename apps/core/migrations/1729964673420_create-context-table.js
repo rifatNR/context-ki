754016d0dffdb9025c9f92c1f4cc9bc53c61c902
RifatNR <Rifatnoor92@gmail.com>
2024-11-09 21:16:00 +0600

    made the migration

--- apps/core/migrations/1729964673420_create-context-table.js
+++ apps/core/migrations/1729964673420_create-context-table.js
@@ -5,13 +5,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-    pgm.createTable("contexts", {
+    pgm.createTable("ideas", {
         id: { type: "serial", primaryKey: true },
-        title: { type: "varchar(255)", notNull: true },
-        context: { type: "text", notNull: true },
+        title: { type: "varchar(255)", unique: true, notNull: true },
+        subtitle: { type: "varchar(255)", unique: false, notNull: false },
         details: { type: "text", unique: false, notNull: false },
         image: { type: "text", unique: false, notNull: false },
+        claimed_date: { type: "timestamp", notNull: false },
         created_at: {
+            type: "timestamp",
+            default: pgm.func("current_timestamp"),
+        },
+        updated_at: {
             type: "timestamp",
             default: pgm.func("current_timestamp"),
         },
@@ -19,5 +24,5 @@ exports.up = (pgm) => {
 };
 
 exports.down = (pgm) => {
-    pgm.dropTable("contexts");
+    pgm.dropTable("ideas");
 };
