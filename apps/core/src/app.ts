b60f1d2db019e5f8dc4422acf709112e8237ea11
RifatNR <Rifatnoor92@gmail.com>
2024-11-09 23:53:24 +0600

    trpc playground is working so far

--- apps/core/src/app.ts
+++ apps/core/src/app.ts
@@ -1,15 +1,43 @@
 import express, { Request, Response } from "express";
 import adminRoutes from "@/routes/admin";
 import { appRouter } from "@/router/router";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { expressHandler } from "trpc-playground/handlers/express";
 
 export type appRouter = typeof appRouter;
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+const trpcApiEndpoint = "/api/trpc";
+const playgroundEndpoint = "/api/trpc-playground";
 
+app.use(express.json());
 app.use("/admin", adminRoutes);
+
+app.use(
+    trpcApiEndpoint,
+    trpcExpress.createExpressMiddleware({
+        router: appRouter,
+    })
+);
+
+const setupPlayground = async () => {
+    app.use(
+        playgroundEndpoint,
+        await expressHandler({
+            trpcApiEndpoint,
+            playgroundEndpoint,
+            router: appRouter,
+            // uncomment this if you're using superjson
+            // request: {
+            //   superjson: true,
+            // },
+        })
+    );
+};
+
+setupPlayground();
 
 app.get("/", async (req: Request, res: Response) => {
     res.json({ msg: "I Thought of it First." });
