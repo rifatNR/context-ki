cb98bb559ac99ec481cde470c29d39b83242b4d0
RifatNR <Rifatnoor92@gmail.com>
2024-11-17 02:25:28 +0600

    Changes

--- apps/web/src/app/patent/create/confirmation/page.tsx
+++ apps/web/src/app/patent/create/confirmation/page.tsx
@@ -6,6 +6,7 @@ import { IoCalendarNumberOutline } from "react-icons/io5";
 import { GiMedal, GiRibbonMedal } from "react-icons/gi";
 import { LiaMedalSolid } from "react-icons/lia";
 import { Checkbox } from "@/components/ui/checkbox";
+import Link from "next/link";
 
 const Confirmation = () => {
     return (
@@ -13,21 +14,32 @@ const Confirmation = () => {
             <div className="flex-1">
                 <div className="text-4xl mb-12 font-bold">Confirmation:</div>
 
-                <div className="text-2xl mt-5">
+                <div className="text-2xl mt-5 mb-5">
                     Once a patent is published, it becomes a permanent record
-                    and cannot be updated or deleted. Please ensure that all
-                    details are final before publication. However, any pending
-                    invites will still be able to join and view the patent after
-                    its publication. Additionally, please be aware that public
-                    patents are openly accessible, meaning that anyone can view
-                    and review the ideas you have disclosed. Consider the
-                    visibility of your intellectual property before proceeding
-                    with publication.
+                    and{" "}
+                    <span className="text-red-400">
+                        cannot be updated or deleted
+                    </span>
+                    . Please ensure that all details are final before
+                    publication. However, any pending invites will still be able
+                    to join and view the patent after its publication.
+                    Additionally, please be aware that public patents are openly
+                    accessible, meaning that anyone can view and review the
+                    ideas you have disclosed. Consider the visibility of your
+                    intellectual property before proceeding with publication.
                 </div>
 
+                <div className="flex items-center space-x-3 text-custom-gray-25">
+                    <Link href={"/terms"}>
+                        <div className="underline">Terms &amp; Conditions</div>
+                    </Link>
+                    <Link href={"/privacy"}>
+                        <div className="underline">Privacy Policy</div>
+                    </Link>
+                </div>
                 <label
                     htmlFor="terms"
-                    className="flex items-center space-x-3 mt-5 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-2xl cursor-pointer"
+                    className="flex items-center mt-4 space-x-3 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-2xl cursor-pointer"
                 >
                     <Checkbox
                         id="terms"
