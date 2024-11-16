b516540dd8831435cd28443d679e61cc7e59bee8
RifatNR <Rifatnoor92@gmail.com>
2024-11-16 18:42:11 +0600

    changes

--- apps/web/src/app/patent/create/title/page.tsx
+++ apps/web/src/app/patent/create/title/page.tsx
@@ -1,13 +1,36 @@
+"use client";
+
 import PrevNextButton from "@/app/patent/create/PrevNextButton";
+import { useRef } from "react";
 
 const Title = () => {
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    const handleInputChange = () => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = "auto";
+            const scrollHeight = textareaRef.current.scrollHeight;
+            const maxHeight = 253;
+
+            if (scrollHeight > maxHeight) {
+                textareaRef.current.style.height = `${maxHeight}px`;
+                textareaRef.current.style.overflowY = "scroll";
+            } else {
+                textareaRef.current.style.height = `${scrollHeight}px`;
+                textareaRef.current.style.overflowY = "hidden";
+            }
+        }
+    };
+
     return (
         <div className="flex-1 flex items-center justify-center w-full -mt-20">
             <div className="flex-1">
                 <div className="text-4xl mb-10">Enter a Title:</div>
 
                 <textarea
-                    className="text-custom-gray-25 bg-transparent w-full text-3xl overflow-hidden resize-none focus:outline-none"
+                    ref={textareaRef}
+                    onInput={handleInputChange}
+                    className="text-custom-gray-25 bg-transparent w-full text-3xl resize-none focus:outline-none"
                     rows={1}
                     placeholder="Enter the title of your idea..."
                 ></textarea>
