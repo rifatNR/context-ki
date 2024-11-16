a21a5e0a0cd6829e6f912350f1ee76d9b2d8b96d
RifatNR <Rifatnoor92@gmail.com>
2024-11-16 23:59:51 +0600

    Updated select ui

--- apps/web/src/app/patent/create/date/page.tsx
+++ apps/web/src/app/patent/create/date/page.tsx
@@ -3,10 +3,33 @@
 import PrevNextButton from "@/app/patent/create/PrevNextButton";
 import { useEffect, useRef } from "react";
 import { VscTriangleDown } from "react-icons/vsc";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
 
 const Date = () => {
     const currentYear = 2025; // TODO:: Fix Static
     const years = Array.from({ length: 20 }, (_, i) => currentYear - i);
+    const months = [
+        "january",
+        "february",
+        "march",
+        "april",
+        "may",
+        "june",
+        "july",
+        "august",
+        "september",
+        "october",
+        "november",
+        "december",
+    ];
 
     return (
         <div className="flex-1 flex items-center justify-center w-full -mt-20">
@@ -17,73 +40,70 @@ const Date = () => {
 
                 <div className="flex items-center justify-stretch w-full space-x-10">
                     <div className="flex-1">
-                        <div className="flex items-center justify-between">
-                            <select
-                                id="underline_select"
-                                className="block py-2.5 px-0 w-full text-3xl text-gray-500 bg-transparent appearance-none focus:outline-none focus:ring-0 peer"
-                            >
-                                <option selected className="bg-black">
-                                    Day
-                                </option>
-                                {[...Array(31)]?.map((item, index) => (
-                                    <option
-                                        key={item}
-                                        value={index + 1}
-                                        className="bg-black"
+                        <Select onValueChange={(value) => console.log(value)}>
+                            <SelectTrigger className="relative w-full border-none bg-transparent text-3xl text-gray-500 focus:ring-0 rounded-none">
+                                <SelectValue placeholder="Day" />
+                                <span className="absolute right-2">
+                                    <VscTriangleDown className="text-xl" />
+                                </span>
+                            </SelectTrigger>
+                            <SelectContent className="rounded-none bg-black text-white mt-1 border-2 mt-1 border-2">
+                                {[...Array(31)].map((item, index) => (
+                                    <SelectItem
+                                        key={index}
+                                        value={(index + 1).toString()}
+                                        className="text-xl rounded-none"
                                     >
                                         {index + 1}
-                                    </option>
+                                    </SelectItem>
                                 ))}
-                            </select>
-                            <VscTriangleDown />
-                        </div>
-                        <div className="w-full bg-white h-0.5 rounded-full motion-scale-x-in-[0] motion"></div>
+                            </SelectContent>
+                        </Select>
+                        <div className="mt-2 h-0.5 w-full rounded-full bg-white motion-scale-x-in-[0] motion"></div>
                     </div>
                     <div className="flex-1">
-                        <div className="flex items-center justify-between">
-                            <select
-                                id="underline_select"
-                                className="block py-2.5 px-0 w-full text-3xl text-gray-500 bg-transparent appearance-none focus:outline-none focus:ring-0 peer"
-                            >
-                                <option selected className="bg-black">
-                                    Month
-                                </option>
-                                {[...Array(31)]?.map((item, index) => (
-                                    <option
-                                        key={item}
-                                        value={index}
-                                        className="bg-black"
+                        <Select onValueChange={(value) => console.log(value)}>
+                            <SelectTrigger className="relative w-full border-none bg-transparent text-3xl text-gray-500 focus:ring-0 rounded-none capitalize">
+                                <SelectValue placeholder="Month" />
+                                <span className="absolute right-2">
+                                    <VscTriangleDown className="text-xl" />
+                                </span>
+                            </SelectTrigger>
+                            <SelectContent className="rounded-none bg-black text-white mt-1 border-2">
+                                {months.map((month) => (
+                                    <SelectItem
+                                        key={month}
+                                        value={month}
+                                        className="text-xl rounded-none capitalize"
                                     >
-                                        {index}
-                                    </option>
+                                        {month}
+                                    </SelectItem>
                                 ))}
-                            </select>
-                            <VscTriangleDown />
-                        </div>
-                        <div className="w-full bg-white h-0.5 rounded-full motion-scale-x-in-[0] motion"></div>
+                            </SelectContent>
+                        </Select>
+                        <div className="mt-2 h-0.5 w-full rounded-full bg-white motion-scale-x-in-[0] motion"></div>
                     </div>
                     <div className="flex-1">
-                        <div className="flex items-center justify-between">
-                            <select
-                                id="underline_select"
-                                className="block py-2.5 px-0 w-full text-3xl text-gray-500 bg-transparent appearance-none focus:outline-none focus:ring-0 peer"
-                            >
-                                <option selected className="bg-black">
-                                    Year
-                                </option>
+                        <Select onValueChange={(value) => console.log(value)}>
+                            <SelectTrigger className="relative w-full border-none bg-transparent text-3xl text-gray-500 focus:ring-0 rounded-none">
+                                <SelectValue placeholder="Year" />
+                                <span className="absolute right-2">
+                                    <VscTriangleDown className="text-xl" />
+                                </span>
+                            </SelectTrigger>
+                            <SelectContent className="rounded-none bg-black text-white mt-1 border-2">
                                 {years.map((year) => (
-                                    <option
+                                    <SelectItem
                                         key={year}
-                                        value={year}
-                                        className="bg-black"
+                                        value={year.toString()}
+                                        className="text-xl rounded-none"
                                     >
                                         {year}
-                                    </option>
+                                    </SelectItem>
                                 ))}
-                            </select>
-                            <VscTriangleDown />
-                        </div>
-                        <div className="w-full bg-white h-0.5 rounded-full motion-scale-x-in-[0] motion"></div>
+                            </SelectContent>
+                        </Select>
+                        <div className="mt-2 h-0.5 w-full rounded-full bg-white motion-scale-x-in-[0] motion"></div>
                     </div>
                 </div>
 
