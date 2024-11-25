4dab461b533ae76751a4e9097c51b4cd77fc68bf
RifatNR <Rifatnoor92@gmail.com>
2024-11-26 03:33:27 +0600

    design tweak

--- apps/web/src/components/layouts/ProfileDropdown.tsx
+++ apps/web/src/components/layouts/ProfileDropdown.tsx
@@ -27,20 +27,20 @@ const ProfileDropdown = ({ children }: PropType) => {
             <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
             <DropdownMenuContent
                 align="end"
-                className="w-56 mt-2 rounded-none p-0 bg-black text-white border-gray-700"
+                className="w-56 mt-2 rounded-none p-0 py-3 bg-black text-white border-2 border-[#353535]"
             >
-                <DropdownMenuLabel className="rounded-none text-xl">
+                <DropdownMenuLabel className="rounded-none text-xl px-5 mb-2">
                     Hi, {user?.displayName ?? "User"}
                 </DropdownMenuLabel>
                 <DropdownMenuGroup>
-                    <DropdownMenuItem className="rounded-none text-lg cursor-pointer">
+                    <DropdownMenuItem className="rounded-none text-lg cursor-pointer px-3">
                         <User />
                         <span>Profile</span>
                     </DropdownMenuItem>
                 </DropdownMenuGroup>
                 <DropdownMenuItem
                     onClick={logout}
-                    className="rounded-none py-2 text-lg cursor-pointer"
+                    className="rounded-none py-2 text-lg cursor-pointer px-3"
                 >
                     <LogOut />
                     <span>Log out</span>
