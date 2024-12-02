a78bbe234acba30be02e39fa36a7ec1798db64b2
RifatNR <Rifatnoor92@gmail.com>
2024-12-03 03:51:46 +0600

    changes

--- apps/web/src/app/Homepage/Testimonial.tsx
+++ apps/web/src/app/Homepage/Testimonial.tsx
@@ -1,13 +1,17 @@
+import QuoteSVG from "@/components/svg/QuoteSVG";
 import React from "react";
 
 const Testimonial = () => {
     return (
-        <section className="my-12">
+        <section className="mt-12 mb-96">
             <h2 className="text-4xl font-bold text-center mb-20">
                 What People Are Saying
             </h2>
             <div className="grid md:grid-cols-3 gap-10">
-                <blockquote className="bg-[#1A1A1B] shadow-md p-6 rounded-lg text-2xl">
+                <blockquote className="bg-[#1A1A1B] border border-[#2c2c2e] shadow-md p-6 rounded-lg text-2xl scale-90">
+                    <div className="w-12 mb-5">
+                        <QuoteSVG />
+                    </div>
                     <p className="italic">
                         &quot;I knew I was ahead of my time, but now I have
                         proof!&quot;
@@ -16,7 +20,10 @@ const Testimonial = () => {
                         - Future Innovator 🧠
                     </footer>
                 </blockquote>
-                <blockquote className="bg-[#1A1A1B] shadow-md p-6 rounded-lg text-2xl">
+                <blockquote className="bg-[#1A1A1B] border border-[#2c2c2e] shadow-md p-6 rounded-lg text-2xl scale-110">
+                    <div className="w-16 mb-5">
+                        <QuoteSVG />
+                    </div>
                     <p className="italic">
                         &quot;Patent Ideas helped me show off at every dinner
                         party. Worth it!&quot;
@@ -25,7 +32,10 @@ const Testimonial = () => {
                         - Idea Hoarder 🎉
                     </footer>
                 </blockquote>
-                <blockquote className="bg-[#1A1A1B] shadow-md p-6 rounded-lg text-2xl">
+                <blockquote className="bg-[#1A1A1B] border border-[#2c2c2e] shadow-md p-6 rounded-lg text-2xl scale-90">
+                    <div className="w-12 mb-5">
+                        <QuoteSVG />
+                    </div>
                     <p className="italic">
                         &quot;Honestly, it’s just fun to see my old ideas. Also,
                         I now have blackmail material.&quot;
---
+++ apps/web/src/components/svg/QuoteSVG.tsx
@@ -0,0 +1,20 @@
+const QuoteSVG = () => {
+    return (
+        <svg
+            viewBox="0 0 16 16"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                fill="#15ca76"
+                d="M9 9v-7h7v7.1c0 4.8-4.5 5.4-4.5 5.4l-0.6-1.4c0 0 2-0.3 2.4-1.9 0.4-1.2-0.4-2.2-0.4-2.2h-3.9z"
+            ></path>
+            <path
+                fill="#15ca76"
+                d="M0 9v-7h7v7.1c0 4.8-4.5 5.4-4.5 5.4l-0.6-1.4c0 0 2-0.3 2.4-1.9 0.4-1.2-0.4-2.2-0.4-2.2h-3.9z"
+            ></path>
+        </svg>
+    );
+};
+
+export default QuoteSVG;
