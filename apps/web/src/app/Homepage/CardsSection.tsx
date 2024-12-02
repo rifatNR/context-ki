0068bbab330e7a7757716f80611a4189232675ce
RifatNR <Rifatnoor92@gmail.com>
2024-12-03 04:46:26 +0600

    cn

--- apps/web/src/app/Homepage/CardsSection.tsx
+++ apps/web/src/app/Homepage/CardsSection.tsx
@@ -31,7 +31,10 @@ const CardsSection = () => {
                 Let us save your brilliance so you can brag later. Here’s how:
             </p>
 
-            <div className="grid md:grid-cols-3 gap-10 text-left mt-20  ">
+            <div
+                ref={observableRef as any}
+                className="grid md:grid-cols-3 gap-10 text-left mt-20"
+            >
                 <BorderHoverEffectCard
                     parentClass={
                         isVisible
@@ -85,70 +88,6 @@ const CardsSection = () => {
                         Prove you thought of it first when it goes viral. 🎤
                     </p>
                 </BorderHoverEffectCard>
-            </div>
-
-            <div
-                ref={observableRef as any}
-                className="grid md:grid-cols-3 gap-10 text-left mt-20"
-            >
-                <div
-                    className={`bg-[#1A1A1B] shadow-md p-6 rounded-lg
-                                ${
-                                    isVisible
-                                        ? "motion-preset-slide-down motion-preset-focus motion-duration-1000"
-                                        : "invisible"
-                                }`}
-                >
-                    <div className="w-16 mb-5">
-                        <BrainSVG />
-                    </div>
-                    <h3 className="font-bold text-3xl mb-2">
-                        1. Share Your Idea
-                    </h3>
-                    <p className="text-lg text-[#89898D]">
-                        Give it a cool title, add juicy details, and timestamp
-                        it. 🧠
-                    </p>
-                </div>
-
-                <div
-                    className={`bg-[#1A1A1B] shadow-md p-6 rounded-lg
-                                ${
-                                    isVisible
-                                        ? "motion-preset-slide-up motion-preset-focus motion-duration-1000"
-                                        : "invisible"
-                                }`}
-                >
-                    <div className="w-16 mb-5">
-                        <LockSVG />
-                    </div>
-                    <h3 className="font-bold text-3xl mb-2">
-                        2. Secure Your Bragging Rights
-                    </h3>
-                    <p className="text-lg text-[#89898D]">
-                        We’ll keep your idea safe in our vault for future glory.
-                        🔒
-                    </p>
-                </div>
-
-                <div
-                    className={`bg-[#1A1A1B] shadow-md p-6 rounded-lg
-                                ${
-                                    isVisible
-                                        ? "motion-preset-slide-down motion-preset-focus motion-duration-1000"
-                                        : "invisible"
-                                }`}
-                >
-                    <div className="w-16 mb-5">
-                        <ProveSVG />
-                    </div>
-                    <h3 className="font-bold text-3xl mb-2">
-                        3. Show Off Later
-                    </h3>
-                    <p className="text-lg text-[#89898D]">
-                        Prove you thought of it first when it goes viral. 🎤
-                    </p>
-                </div>
             </div>
         </section>
     );
