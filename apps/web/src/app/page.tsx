8af139d06649b7cc1c5698a365fedd0c050de816
RifatNR <Rifatnoor92@gmail.com>
2024-12-03 01:40:52 +0600

    Changes

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -18,13 +18,16 @@ const Home = () => {
             <div className="z-body">
                 <header className="max-w-[1600px] mx-auto h-[100vh] text-white p-10 flex items-center justify-between">
                     <div className="my-auto">
-                        <h1 className="text-7xl font-bold gradient-hero-text pb-2">
+                        <h1
+                            className="text-7xl font-bold gradient-hero-text pb-2
+                                        motion-preset-slide-down motion-preset-focus motion-duration-1000"
+                        >
                             Patent Your <br /> Crazy Ideas (Kinda)
                         </h1>
-                        <p className="text-4xl mt-10">
+                        <p className="text-4xl mt-10 motion-preset-slide-right motion-preset-focus motion-duration-1000">
                             Because being first is all that matters. 🚀✨
                         </p>
-                        <div className="mt-10">
+                        <div className="mt-10 motion-preset-slide-up motion-preset-focus motion-duration-1000">
                             <Link href={"/patent"}>
                                 <button className="hover:scale-105 transition-all ease-linear gradient-button text-xl text-white rounded-lg font-bold">
                                     <div className="gradient-button-content px-5 py-3">
@@ -35,8 +38,12 @@ const Home = () => {
                         </div>
                     </div>
 
-                    <div className="pr-48">
-                        <div className="tilt-box-container relative rounded-lg h-[400px] w-[300px] text-black">
+                    <div className="pr-48 motion-preset-slide-down motion-preset-focus motion-duration-1000">
+                        <div
+                            className="tilt-box-container
+                                    relative hover:scale-110 transition-all duration-300 ease-out
+                                    rounded-lg h-[400px] w-[300px] text-black"
+                        >
                             <div className="t_over"></div>
                             <div className="t_over"></div>
                             <div className="t_over"></div>
@@ -46,9 +53,9 @@ const Home = () => {
                             <div className="t_over"></div>
                             <div className="t_over"></div>
                             <div className="t_over"></div>
-                            <div className="tilt-box relative transition-all duration-300 ease-in h-full w-full bg-white">
+                            <div className="tilt-box relative transition-all duration-300 ease-out h-full w-full bg-white">
                                 <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-7xl">
-                                    HI
+                                    HI <br /> This is not working
                                 </div>
                             </div>
                         </div>
@@ -69,7 +76,10 @@ const Home = () => {
                             Here’s how:
                         </p>
                         <div className="grid md:grid-cols-3 gap-10 text-left mt-20">
-                            <div className="bg-[#1A1A1B] shadow-md p-6 rounded-lg">
+                            <div
+                                className="bg-[#1A1A1B] shadow-md p-6 rounded-lg
+                                            motion-preset-slide-down motion-preset-focus motion-duration-1000"
+                            >
                                 <div className="w-16 mb-5">
                                     <BrainSVG />
                                 </div>
@@ -81,7 +91,10 @@ const Home = () => {
                                     timestamp it. 🧠
                                 </p>
                             </div>
-                            <div className="bg-[#1A1A1B] shadow-md p-6 rounded-lg">
+                            <div
+                                className="bg-[#1A1A1B] shadow-md p-6 rounded-lg
+                                            motion-preset-slide-up motion-preset-focus motion-duration-1000"
+                            >
                                 <div className="w-16 mb-5">
                                     <LockSVG />
                                 </div>
@@ -93,7 +106,10 @@ const Home = () => {
                                     future glory. 🔒
                                 </p>
                             </div>
-                            <div className="bg-[#1A1A1B] shadow-md p-6 rounded-lg">
+                            <div
+                                className="bg-[#1A1A1B] shadow-md p-6 rounded-lg
+                                            motion-preset-slide-down motion-preset-focus motion-duration-1000"
+                            >
                                 <div className="w-16 mb-5">
                                     <ProveSVG />
                                 </div>
